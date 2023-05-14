ca794affcc65443d5ad1899e8759e70573987d2f
Almaha <mahaw999@gmail.com>
2023-05-14 18:30:16 +0300

    create dates module

---
+++ src/client/js/dates.js
@@ -0,0 +1,26 @@
+//check the validity of date entered
+const std = document.querySelector('#startDate')
+const end = document.querySelector('#endDate')
+const dateErr = document.querySelector('#date-error')
+let today = new Date()
+
+std.addEventListener('change', () => {
+  if(std.value < changeDateFormat(today)){
+        dateErr.classList.remove('hide')
+        dateErr.innerHTML = `Cann't start your trip in the past!`
+    } else {
+        dateErr.classList.add('hide')
+    }
+})
+
+end.addEventListener('change', () => {
+     if(end.value < std.value){
+        dateErr.classList.remove('hide')
+        dateErr.innerHTML = 'Wrong dates enteries'
+    } else if(std.value === end.value){
+        dateErr.classList.remove('hide')
+        dateErr.innerHTML = `Are you passing by!`
+    }  else {
+        dateErr.classList.add('hide')
+    }
+})
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -234,27 +234,3 @@ function changeDateFormat(date) {
     return formatedDate
 }
 
-//check the validity of date entered
-const dateErr = document.querySelector('#date-error')
-let today = new Date()
-
-std.addEventListener('change', () => {
-  if(std.value < changeDateFormat(today)){
-        dateErr.classList.remove('hide')
-        dateErr.innerHTML = `Cann't start your trip in the past!`
-    } else {
-        dateErr.classList.add('hide')
-    }
-})
-
-end.addEventListener('change', () => {
-     if(end.value < std.value){
-        dateErr.classList.remove('hide')
-        dateErr.innerHTML = 'Wrong dates enteries'
-    } else if(std.value === end.value){
-        dateErr.classList.remove('hide')
-        dateErr.innerHTML = `Are you passing by!`
-    }  else {
-        dateErr.classList.add('hide')
-    }
-})
