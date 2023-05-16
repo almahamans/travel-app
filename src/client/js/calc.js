117b49def363638a2ebc529c4449cd5322685f5d
Almaha <mahaw999@gmail.com>
2023-05-16 22:46:04 +0300

    adding calcs modul

---
+++ src/client/js/calc.js
@@ -0,0 +1,19 @@
+ //counting 
+ function countdownDays(startDate) {
+    let dthen= new Date(startDate),
+     dnow = new Date()
+ 
+     return Math.floor((Date.UTC(dthen.getFullYear(), dthen.getMonth(), dthen.getDate()) 
+     - Date.UTC(dnow.getFullYear(), dnow.getMonth(), dnow.getDate()))
+      / (1000 * 60 * 60 * 24))
+ }
+ 
+ function daysLong(startDate, endDate){
+     let dthen= new Date(startDate),
+     dend = new Date(endDate)
+     
+     return Math.floor((Date.UTC(dend.getFullYear(), dend.getMonth(), dend.getDate())
+     - Date.UTC(dthen.getFullYear(), dthen.getMonth(), dthen.getDate()))
+      / (1000 * 60 * 60 * 24)
+      ) + 1
+ }
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -199,25 +199,6 @@ document.querySelector(`#r_all`).addEventListener('click', (event) => {
     clearAll()
     event.target.classList.add('hide')
  })
- //counting 
- function countdownDays(startDate) {
-    let dthen= new Date(startDate),
-     dnow = new Date()
- 
-     return Math.floor((Date.UTC(dthen.getFullYear(), dthen.getMonth(), dthen.getDate()) 
-     - Date.UTC(dnow.getFullYear(), dnow.getMonth(), dnow.getDate()))
-      / (1000 * 60 * 60 * 24))
- }
- 
- function daysLong(startDate, endDate){
-     let dthen= new Date(startDate),
-     dend = new Date(endDate)
-     
-     return Math.floor((Date.UTC(dend.getFullYear(), dend.getMonth(), dend.getDate())
-     - Date.UTC(dthen.getFullYear(), dthen.getMonth(), dthen.getDate()))
-      / (1000 * 60 * 60 * 24)
-      ) + 1
- }
 //cahnge date formate to easy checking
 function changeDateFormat(date) {
     let year = date.getFullYear();
